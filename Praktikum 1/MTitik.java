a80e30c4e474427a3107994c6dd78ea6aee2e0a7
Rachmadr <126948977+Rachmadr@users.noreply.github.com>
2024-02-21 11:33:34 +0700

    Mengumpulkan praktikum 1

---
+++ Praktikum 1/MTitik.java
@@ -0,0 +1,27 @@
+public class MTitik {
+    public static void main(String[] args){
+        Titik t1; 
+        Titik t2;
+        //float counterTitik;
+
+        t1 = new Titik();
+        t2 = new Titik();
+
+        t1.setAbsis(6);
+        t1.setOrdinat(7);
+        t2.setAbsis(1);
+        t2.setOrdinat(4);
+
+        Titik t3;
+        t3 = new Titik(5, 6);
+
+        float absis, ordinat;
+        absis = t1.getAbsis();
+        ordinat = t1.getOrdinat();
+
+        System.out.println("Jumlah objek titik :" + Titik.getCounterTitik());
+        System.out.printf("t1(%.1f, %.1f)\n", absis, ordinat);
+        System.out.println("t2(" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
+        System.out.println("t3(" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
+    }    
+}
