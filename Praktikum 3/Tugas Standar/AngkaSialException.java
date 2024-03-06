723c9ac026837055494e6a3c4ffacf43a6446327
Rachmadr <126948977+Rachmadr@users.noreply.github.com>
2024-03-06 11:35:03 +0700

    Add files via upload

---
+++ Praktikum 3/Tugas Standar/AngkaSialException.java
@@ -0,0 +1,10 @@
+// Nama         : Rachmad Rifa'i
+// NIM          : 24060122120014
+// Tanggal      : 06-03-2024
+// Deskripsi    : Kelas yang berisi program exception buatan
+
+public class AngkaSialException extends Exception {
+    public AngkaSialException(){
+        super("JANGAN MASUKKAN ANGKA 13 KARENA ANGKA SIAL!!!");
+    }
+}
